8278bd05e45fece7499752862992af58994fbf24
pavelbeard <pavelborodin1995@gmail.com>
2022-12-02 08:10:34 +0300

    v0.0.7 - дизайн агента радикально изменяется!

--- agent/agent/tests.py
+++ agent/agent/tests.py
@@ -1,31 +1,57 @@
 import asyncio
 import concurrent.futures
 import json
+import os.path
+import threading
 import unittest
 from typing import Coroutine, Any
 
-from .agent import get_cpu_usage, get_ram_usage, get_disk_usage
+import yaml
+
+
+# from .agent import get_cpu_usage, get_ram_usage, get_disk_usage
 
 
 def coro(name):
     return json.dumps({name: "test"})
 
 
-class CoroTests(unittest.TestCase):
-    def test_coroutines(self):
-        result = get_ram_usage(1)
-        self.assertEqual(type(result), type(Coroutine[Any, Any, str]))
+# class CoroTests(unittest.TestCase):
+#     def test_coroutines(self):
+#         result = get_ram_usage(1)
+#         self.assertEqual(type(result), type(Coroutine[Any, Any, str]))
+#
+#     def test_coroutines_2(self):
+#         r = coro()
+#         self.assertEqual(r, type(Coroutine))
+#
+#     def test_run_in_executor(self):
+#         async def run_in_exec():
+#             executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+#             loop = asyncio.get_event_loop()
+#             result = await loop.run_in_executor(executor, coro, "test")
+#             print(result)
+#
+#         asyncio.run(run_in_exec())
 
-    def test_coroutines_2(self):
-        r = coro()
-        self.assertEqual(r, type(Coroutine))
+class ThreadingTest(unittest.TestCase):
+    def test_threading(self):
+        def random_test(key, iteration):
+            from random import random
+            for each in range(2 * 100000):
+                lock = threading.Lock()
 
-    def test_run_in_executor(self):
-        async def run_in_exec():
-            executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-            loop = asyncio.get_event_loop()
-            result = await loop.run_in_executor(executor, coro, "test")
-            print(result)
+                lock.acquire(blocking=True, timeout=5.0)
+                print(f"[{key}] [{each}]")
 
-        asyncio.run(run_in_exec())
+                eacheach = each * each
+                print(eacheach)
 
+                lock.release()
+
+        with open(os.path.abspath('../agent-config.yml'), 'r') as cnf:
+            config: dict = yaml.safe_load(cnf).get('hosts')
+
+            for key, iteration in zip(config.keys(), range(len(config.keys()))):
+                thread = threading.Thread(target=random_test, args=(key, iteration))
+                thread.start()
--- agent/main.py
+++ agent/main.py
@@ -1,31 +1,25 @@
-import asyncio
-import logging
 import os
+import threading
 
-from agent import get_disk_usage, get_ram_usage, get_cpu_usage
-from agent import send_data
+import yaml
+
+CONFIG = {}
 
 
-def setup_logger():
-    log_dir = os.path.join(os.getcwd(), 'log')
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
+def startup():
+    with open(os.path.join(os.getcwd(), 'agent-config.yml'), 'r') as config:
+        global CONFIG
+        CONFIG = yaml.safe_load(config).get('hosts')
 
-    logging.basicConfig(
-        filename=os.path.join(log_dir, 'agent.log'), level=logging.INFO, filemode='a',
-        format="%(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-    )
-    logger = logging.getLogger(__name__)
+
+def gen(key: str, i: int):
+    import random
+    print(random.random().hex(), f"[{key}] - [{i}]")
 
 
 if __name__ == '__main__':
-    setup_logger()
+    startup()
 
-    tasks = (
-        (get_disk_usage, '/api/disk'),
-        (get_ram_usage, '/api/ram', 1),
-        (get_cpu_usage, '/api/cpu'),
-        # network interfaces
-    )
-
-    asyncio.run(send_data(url='http://localhost:8000', data=tasks))
+    for each, i in zip(CONFIG.keys(), range(len(CONFIG.keys()))):
+        thread = threading.Thread(target=gen, args=(each, i))
+        thread.run()
